d4df3304feacd3ae41da1f19d5c18a40fb4dc82e
Mohammed Abd Elhammed <m7mdmmom@gmail.com>
2026-09-10 16:59:50 +0300

    Remove user creation for owner and student roles
    Removed user creation for 'owner' and 'student' roles from AdminSeeder.

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -15,36 +15,5 @@ class AdminSeeder extends Seeder
             'email'    => 'admin1@housing.com',
             'password' => Hash::make('password123'),
         ]);
-        
-        User::create([ 
-            'fname' => 'Mohamed', 
-            'lname' => 'Hassan',
-            'email' => 'owner@owner.com',
-            'password' => Hash::make('password123'),
-            'phone' => '01010000004',
-            'gender' => 'male',
-            'profile_image' => null,
-            'role' => 'owner',
-            'status' => 'active', 
-            'national_id' => '30404040404040',
-            'national_id_image' => null,
-            'email_verified_at' => now(),
-         ]);
-
-User::create([
-           'fname' => 'Ahmed',
-           'lname' => 'Mohamed',
-           'email' => 'ahmed@student.com',
-           'password' => Hash::make('password123'),
-           'phone' => '01010000001',
-           'gender' => 'male', 
-           'profile_image' => null,
-           'role' => 'student', 
-           'status' => 'active',
-           'national_id' => '30101010101010', 
-           'national_id_image' => null,
-           'email_verified_at' => now(),
-        ]);
-        
     }
 }
